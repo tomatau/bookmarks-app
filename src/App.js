390ec6d33711cba34f9e8c26a9cc8eb49e7a7b75
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2018-12-02 17:42:46 +0000

    implements the delete button using context

--- src/App.js
+++ src/App.js
@@ -26,6 +26,17 @@ class App extends Component {
     })
   }
 
+  deleteBookmark = bookmarkId => {
+    console.log(bookmarkId)
+    // todo: remove bookmark with bookmarkId from state
+    const newBookmarks = this.state.bookmarks.filter(bm =>
+      bm.id !== bookmarkId
+    )
+    this.setState({
+      bookmarks: newBookmarks
+    })
+  }
+
   componentDidMount() {
     fetch(config.API_ENDPOINT, {
       method: 'GET',
@@ -48,6 +59,7 @@ class App extends Component {
     const contextValue = {
       bookmarks: this.state.bookmarks,
       addBookmark: this.addBookmark,
+      deleteBookmark: this.deleteBookmark,
     }
     return (
       <main className='App'>
--- src/BookmarkItem/BookmarkItem.js
+++ src/BookmarkItem/BookmarkItem.js
@@ -1,33 +1,67 @@
 import React from 'react';
 import Rating from '../Rating/Rating';
+import BookmarksContext from '../BookmarksContext';
+import config from '../config';
 import './BookmarkItem.css';
+
+function deleteBookmarkRequest(bookmarkId, cb) {
+  fetch(config.API_ENDPOINT + `/${bookmarkId}`, {
+    method: 'DELETE',
+  })
+    .then(res => {
+      if (!res.ok) {
+        // get the error message from the response,
+        return res.json().then(error => {
+          // then throw it
+          throw error
+        })
+      }
+      return res.json()
+    })
+    .then(data => {
+      console.log({ data })
+      cb(bookmarkId)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
 
 export default function BookmarkItem(props) {
   return (
-    <li className='BookmarkItem'>
-      <div className='BookmarkItem__row'>
-        <h3 className='BookmarkItem__title'>
-          <a
-            href={props.url}
-            target='_blank'
-            rel='noopener noreferrer'>
-            {props.title}
-          </a>
-        </h3>
-        <Rating value={props.rating} />
-      </div>
-      <p className='BookmarkItem__description'>
-        {props.description}
-      </p>
-      <div className='BookmarkItem__buttons'>
-        <button
-          className='BookmarkItem__description'
-          onClick={() => props.onClickDelete(props.id)}
-        >
-          Delete
-        </button>
-      </div>
-    </li>
+    <BookmarksContext.Consumer>
+      {(context) => (
+        <li className='BookmarkItem'>
+          <div className='BookmarkItem__row'>
+            <h3 className='BookmarkItem__title'>
+              <a
+                href={props.url}
+                target='_blank'
+                rel='noopener noreferrer'>
+                {props.title}
+              </a>
+            </h3>
+            <Rating value={props.rating} />
+          </div>
+          <p className='BookmarkItem__description'>
+            {props.description}
+          </p>
+          <div className='BookmarkItem__buttons'>
+            <button
+              className='BookmarkItem__description'
+              onClick={() => {
+                deleteBookmarkRequest(
+                  props.id,
+                  context.deleteBookmark,
+                )
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        </li>
+      )}
+    </BookmarksContext.Consumer>
   )
 }
 
