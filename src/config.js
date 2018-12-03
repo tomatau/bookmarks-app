58e4742856409c42515f53dbbbe83cf74f6b30e5
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2018-12-03 12:41:37 +0000

    setup deploy scripts in package json and read API_KEY from env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
